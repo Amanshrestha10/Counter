4561f5f9dcbeb46565e021a9a0d916f4f5da45b3
Amanshrestha10 <151425995+Amanshrestha10@users.noreply.github.com>
2024-07-28 20:53:46 +0545

    Add files via upload

---
+++ counterapp.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Counter from './counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
